be24ba2bac4f434deda62531b836445bd569f001
ChickenMand <tangohoteloscarromeo@gmail.com>
2019-12-16 10:32:53 +0100

    Fikset dit lort

--- Data_Behandling.py
+++ Data_Behandling.py
@@ -8,22 +8,22 @@ def setup():
     #og konverteres til en liste:
     data = list(reader)
 
-    cmd = ''
+    cmd = ""
 
-    # def getBobs():
-    #     totalBombs = 0
-    #     numberOfMissions = 0
-    #
-    #     #for-løkken gennemløber hele datasættet
-    #     for row in data:
-    #         #Den kolonne der hedder "PlanetaryMassJpt" indeholde massen, målt i enheden "jupitermasser".
-    #         #Hvis massen ikke findes i datasættet får m værdien '' (En tom streng).
-    #         #Derfor er vi nødt til at undersøge om m har en gyldig værdi,
-    #         #før vi tæller planeten med:
-    #         if row['High Explosives'] != '':
-    #             m = float(row['High Explosives'])
-    #             numberOfMissions += 1
-    #             totalBombs += m
+def getBobs():
+    totalBombs = 0
+    numberOfMissions = 0
+
+    #for-løkken gennemløber hele datasættet
+    for row in data:
+        #Den kolonne der hedder "PlanetaryMassJpt" indeholde massen, målt i enheden "jupitermasser".
+        #Hvis massen ikke findes i datasættet får m værdien '' (En tom streng).
+        #Derfor er vi nødt til at undersøge om m har en gyldig værdi,
+        #før vi tæller planeten med:
+        if row['High Explosives'] != '':
+            m = float(row['High Explosives'])
+            numberOfMissions += 1
+            totalBombs += m
 
 
         # # Nu kan gennemsnittet udregnes:
